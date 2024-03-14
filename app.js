d2e95dadf11d489e0c4d90185da8bd469ce45d40
Geovanna <125594657+GeovannaNascimento@users.noreply.github.com>
2024-03-14 14:10:32 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import { View, Text, Image, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      {/* Cabeçalho */}
+      <View style={styles.header}>
+        {/* Imagem 1 */}
+        <Image style={styles.logo} source={require('./icon.png')} />
+        {/* Imagem 2 */}
+        <Image style={styles.logo} source={require('./write.png')} />
+      </View>
+      {/* Contêiner central para o formulário de login */}
+      <View style={styles.container}>
+        {/* Imagem de Login */}
+        <Image style={styles.loginImage} source={require('./login.png')} />
+        {/* Campo de entrada para o email */}
+        <TextInput style={styles.input} placeholder="Email" />
+        {/* Campo de entrada para a senha */}
+        <TextInput style={styles.input} placeholder="Senha" secureTextEntry={true} />
+        {/* Botão de confirmação */}
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Confirmar</Text>
+        </TouchableOpacity>
+      </View>
+      {/* Rodapé */}
+      <View style={styles.footer}>
+        {/* Imagem "insta" */}
+        <Image style={styles.instaImage} source={require('./insta.png')} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+    position: 'relative',
+  },
+  header: {
+    width: '100%',
+    height: 60,
+    backgroundColor: 'transparent',
+    position: 'absolute',
+    top: 0,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingLeft: 20,
+    borderBottomWidth: 3,
+    borderBottomColor: '#dcdcdc',
+  },
+  footer: {
+    width: '100%',
+    height: 50,
+    backgroundColor: 'transparent',
+    position: 'absolute',
+    bottom: 0,
+    borderTopWidth: 3,
+    borderTopColor: '#dcdcdc',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo: {
+    width: 40,
+    height: 40,
+    marginRight: 10,
+  },
+  loginImage: {
+    width: 100,
+    height: 100,
+    alignSelf: 'center',
+  },
+  instaImage: {
+    width: 50,
+    height: 50,
+  },
+  input: {
+    width: '100%',
+    padding: 10,
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    boxSizing: 'border-box',
+  },
+  button: {
+    width: '100%',
+    padding: 10,
+    backgroundColor: '#ffcccc',
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#ffffff',
+  },
+});
+
+export default App;
